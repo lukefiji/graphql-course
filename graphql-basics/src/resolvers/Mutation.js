09aafb81138c89ace19f2cba3d44e3fe304dad66
Luke-Fiji <luke.fiji@coxautoinc.com>
2018-11-05 14:56:29 -0800

    Complete video 36 - setting up comments subscription

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -127,7 +127,7 @@ const Mutation = {
 
     return post;
   },
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
     const postExists = db.posts.some(
       post => post.id === args.data.post && post.published
@@ -147,6 +147,12 @@ const Mutation = {
     };
 
     db.comments.push(comment);
+
+    // First argument: Channel
+    // Second argument : What to publish to channel - must match schema
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment
+    });
 
     return comment;
   },
